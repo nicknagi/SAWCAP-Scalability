cd6fc1231ca0868b3e26f8cd6cdb9e62b4dfef20
Nekhil Nagia <nekhilnagia16@gmail.com>
2020-11-23 17:43:04 +0000

    use get_stacks to get data from now on

--- slave code/get_stacks.sh
+++ slave code/get_stacks.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+data_dir="/home/ubuntu/data"
+
 if [ "$#" -ne 1 ]; then
         echo 'Usage monitor.sh <sampling interval in sec>'
         exit
@@ -18,16 +20,16 @@ while true; do
 		data=`top -p $pid -b1 -n 1|tail -1|awk '{print $9","$10}'`
 		#check if the process died in the middle
 		if [[ $data == *"CPU"* ]]; then
-			echo '0,0'>> resource_data
+			echo '0,0'>> "${data_dir}/resource_data"
 		else		
-			echo $data >> resource_data
+			echo $data >> "${data_dir}/resource_data"
 		fi
 
 		#get stacktrace info
-		jstack $pid > temp_threaddump
-		cat temp_threaddump | java -jar jtda-cli.jar > threaddump_aggregate_$count
-		sed -i 's/<0x[0-9a-zA-Z]*>//g' threaddump_aggregate_$count
-		rm -f temp_threaddump
+		jstack $pid > "${data_dir}/temp_threaddump"
+		cat "${data_dir}/temp_threaddump" | java -jar jtda-cli.jar > "${data_dir}/threaddump_aggregate_${count}"
+		sed -i 's/<0x[0-9a-zA-Z]*>//g' "${data_dir}/threaddump_aggregate_${count}"
+		rm -f "${data_dir}/temp_threaddump"
 		count=$((count+1))
 	fi
 
