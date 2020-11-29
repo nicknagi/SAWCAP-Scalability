d77e022bee4b2dd23ca3a13ff7dcbc031ead5fbf
Nekhil Nagia <nekhilnagia16@gmail.com>
2020-11-29 13:35:53 -0500

    Removed DataAggregator class and replaces with functions

--- sawcap/data_aggregator/data_aggregator.py
+++ sawcap/data_aggregator/data_aggregator.py
@@ -1,48 +1,37 @@
-import os
+def generate_histograms_and_unique_stacktraces(stacktrace_data):
+    histograms = _build_histogram(stacktrace_data)
 
-class DataAggregator:
-    '''
-    input_data: {resource_usage: [[1,2], [10,11] ...], stacktraces: [A, B, C, D...]} # Should be changed to a window object later on
-    input_data is essentially the snapshot that we need to aggregate
-    '''
+    unique_stacktraces = list(histograms.keys())
+    thread_histograms = list(histograms.values())
 
-    def __init__(self, snapshot_collection):
-        self._snapshot_collection = snapshot_collection
+    return unique_stacktraces, thread_histograms
 
-    def generate_histograms_and_unique_stacktraces(self):
-        histograms = self._build_histogram(self._snapshot_collection.stacktrace_data)
+def _build_histogram(stacktrace_data):
+    histograms = {}
+    for stacktrace_dump in stacktrace_data:
+        functions, counts = _extract_functions_and_counts(stacktrace_dump)
+        assert(len(functions) == len(counts))
 
-        unique_stacktraces = list(histograms.keys())
-        thread_histograms = list(histograms.values())
+        for function, freq in zip(functions, counts):
+            if function in histograms:
+                # Might not want to do this as over multiple dumps this value will add up [will take care of this later]
+                histograms[function] += freq
+            else:
+                histograms[function] = freq
+    return histograms
 
-        return unique_stacktraces, thread_histograms
+def _extract_functions_and_counts(stacktrace):
+    # find all the empty lines and split
+    stacktrace_chunks = stacktrace.split('\n\n')[1:]
+    stacks = []
+    counts = []
 
-    def _build_histogram(self, stacktrace_data):
-        histograms = {}
-        for stacktrace_dump in stacktrace_data:
-            functions, counts = self._extract_functions_and_counts(stacktrace_dump)
-            assert(len(functions) == len(counts))
+    for chunk in stacktrace_chunks:
+        if "Stack:\n" not in chunk:
+            continue
+        stack = chunk.split("Stack:\n")[1]
+        if "spark" in stack:
+            counts.append(int(chunk.split("States:")[0].split(" ")[0]))
+            stacks.append(stack)
 
-            for function, freq in zip(functions, counts):
-                if function in histograms:
-                    # Might not want to do this as over multiple dumps this value will add up [will take care of this later]
-                    histograms[function] += freq
-                else:
-                    histograms[function] = freq
-        return histograms
-
-    def _extract_functions_and_counts(self, stacktrace):
-        # find all the empty lines and split
-        stacktrace_chunks = stacktrace.split('\n\n')[1:]
-        stacks = []
-        counts = []
-
-        for chunk in stacktrace_chunks:
-            if "Stack:\n" not in chunk:
-                continue
-            stack = chunk.split("Stack:\n")[1]
-            if "spark" in stack:
-                counts.append(int(chunk.split("States:")[0].split(" ")[0]))
-                stacks.append(stack)
-
-        return stacks, counts
+    return stacks, counts
