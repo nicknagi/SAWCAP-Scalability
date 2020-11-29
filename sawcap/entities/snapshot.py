b16ed09f3c67ed0f96be34940dcf61414650bd8d
Nekhil Nagia <nekhilnagia16@gmail.com>
2020-11-29 01:02:42 -0500

    added stacktrace related metrics template

--- sawcap/entities/snapshot.py
+++ sawcap/entities/snapshot.py
@@ -8,5 +8,11 @@ class Snapshot:
         self.stacktrace_data = stacktrace_data
         self.resource_aggregation = [] # Will replace with a function call in DataAggregator later
 
-        # data_aggregator = DataAggregator(self)
-        # self.stacktrace_aggregation = data_aggregator.generate_histograms_and_unique_stacktraces()
+        data_aggregator = DataAggregator(self)
+        self.stacktrace_aggregation = data_aggregator.generate_histograms_and_unique_stacktraces()
+
+    def stacktrace_similarity(self, other_snapshot):
+        pass
+
+    def threadcount_similarity(self, other_snapshot):
+        pass
