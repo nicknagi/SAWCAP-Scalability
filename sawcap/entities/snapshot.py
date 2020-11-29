a3e17d3bb9e25995c3a15eca08e48b8e8b2e350e
Nekhil Nagia <nekhilnagia16@gmail.com>
2020-11-29 02:17:49 -0500

    modified return structure of data aggregator due to reinterpretation of paper, added stacktrace related metrics; Still need to add new unittests

--- sawcap/entities/snapshot.py
+++ sawcap/entities/snapshot.py
@@ -1,4 +1,5 @@
 from data_aggregator.data_aggregator import DataAggregator
+import math
 
 # Class for representing a snapshot entity
 class Snapshot:
@@ -9,10 +10,30 @@ class Snapshot:
         self.resource_aggregation = [] # Will replace with a function call in DataAggregator later
 
         data_aggregator = DataAggregator(self)
-        self.stacktrace_aggregation = data_aggregator.generate_histograms_and_unique_stacktraces()
+        self.unique_stacktraces, self.thread_histograms = data_aggregator.generate_histograms_and_unique_stacktraces()
 
+    # the current snapshot is treated as the reference point
     def stacktrace_similarity(self, other_snapshot):
-        pass
+        common_stacktraces = set(self.unique_stacktraces).intersection(other_snapshot.unique_stacktraces)
+        return len(common_stacktraces) / len(self.unique_stacktraces)
 
     def threadcount_similarity(self, other_snapshot):
-        pass
+        common_stacktraces = set(self.unique_stacktraces).intersection(other_snapshot.unique_stacktraces)
+        common_thread_histogram_current = []
+        common_thread_histogram_other = []
+
+        for index, stacktrace in enumerate(self.unique_stacktraces):
+            if stacktrace in common_stacktraces:
+                common_thread_histogram_current.append(self.thread_histograms[index])
+
+        for index, stacktrace in enumerate(other_snapshot.unique_stacktraces):
+            if stacktrace in common_stacktraces:
+                common_thread_histogram_other.append(other_snapshot.thread_histograms[index])
+        
+        assert(len(common_thread_histogram_current) == len(common_thread_histogram_other))
+
+        threadcount_similarity = 0
+
+        for h_curr, h_other in zip(common_thread_histogram_current, common_thread_histogram_other):
+            # Squared-Chord Distance
+            threadcount_similarity += ((math.sqrt(h_curr) - math.sqrt(h_other)) ** 2)
