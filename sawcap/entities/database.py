ddb16e60328db83fa807c024cdff4f17f10f05d1
Nekhil Nagia <nekhilnagia16@gmail.com>
2020-12-01 20:00:57 -0500

    Add new Database, Workload Classes (#15)
    * added Database and Workload class, started sawcap entrypoint file
    
    * updates on database, workload classes
    
    * minor changes
    
    * more minor changes
    
    * more minor changes

---
+++ sawcap/entities/database.py
@@ -0,0 +1,54 @@
+from collections import defaultdict
+import uuid
+
+# Class assumes workload class exists
+
+'''
+Structure of the database dictionary:
+    {
+        "characterization_queue": [workload3],
+        "workload_classes": {
+            "uuid4-1" : {
+                "workloads": [workload1, workload4],
+                "model": model-key -- predictor maintains internal data_structure that stores the models
+            },
+            "uuid4-2" : {
+                "workloads": [workload2],
+                "model": model-key
+            }
+        }
+    }
+'''
+class Database:
+    def __init__(self):
+        self._database = dict()
+        self._database["characterization_queue"] = list()
+        self._database["workload_classes"] = dict()
+
+    def add_new_workload(self, workload):
+        self._database["characterization_queue"].append(workload)
+    
+    def create_new_workload_class(self):
+        new_class_id = str(uuid.uuid4())
+        self._database["workload_classes"][new_class_id] = dict()
+        self._database["workload_classes"][new_class_id]["workloads"] = list()
+
+        return new_class_id
+    
+    def add_workload_to_class(self, workload, class_id):
+        self._database["workload_classes"][class_id]["workloads"].append(workload)
+    
+    def add_model_to_workload_class(self, class_id, model_id):
+        self._database["workload_classes"][class_id]["model"] = model_id
+
+    def get_uncharacterized_workloads(self):
+        return self._database["characterization_queue"]
+    
+    def get_model_id_from_class(self, workload_class_id):
+        return self._database["workload_classes"][workload_class_id]["model"]
+    
+    def get_all_workload_classes(self):
+        return list(self._database["workload_classes"].keys())
+
+    def get_workloads_from_workload_class(self, workload_class_id):
+        return self._database["workload_classes"][workload_class_id]["workloads"]
---
+++ sawcap/sawcap.py
@@ -0,0 +1,20 @@
+# The entrypoint for the sawcap project - this should be run on the runner i.e the node that monitors the cluster
+# get_stacks.sh will be run on each of the workers
+
+class Sawcap:
+    
+    def __init__(self):
+        self.database = Database()
+        self.data_collector = DataCollector()
+
+    def run():
+        while True:
+            # The logic for running everything
+
+            pass
+
+
+if __name__ == "__main__":
+    sawcap = Sawcap()
+    sawcap.run()
+    
