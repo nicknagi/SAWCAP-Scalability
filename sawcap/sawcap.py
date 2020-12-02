130497b2d9c61447fa1ee72d0b75f9dbefd7be49
Nekhil Nagia <nekhilnagia16@gmail.com>
2020-12-01 20:18:12 -0500

    testing data collection

--- sawcap/sawcap.py
+++ sawcap/sawcap.py
@@ -1,20 +1,21 @@
 # The entrypoint for the sawcap project - this should be run on the runner i.e the node that monitors the cluster
 # get_stacks.sh will be run on each of the workers
+from entities.database import Database
+from data_collector.collector import DataCollector
+from time import sleep
 
 class Sawcap:
-    
     def __init__(self):
         self.database = Database()
-        self.data_collector = DataCollector()
+        self.data_collector = DataCollector(["localhost"])
 
     def run():
         while True:
             # The logic for running everything
-
-            pass
-
+            x = self.data_collector.get_new_data(10)
+            print(x)
+            sleep(11)
 
 if __name__ == "__main__":
     sawcap = Sawcap()
     sawcap.run()
-    
