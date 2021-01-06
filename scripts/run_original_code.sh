4f7e1962f1b5a3f37b1bfba11c16dedef9c3b9ad
Nekhil Nagia <nekhilnagia16@gmail.com>
2021-01-06 01:00:38 +0000

    added script for running original code

---
+++ scripts/run_original_code.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+code_path='/home/ubuntu/capstone/archive/original/master code'
+file_path='/home/ubuntu/data'
+calculate_error=1	# 0 or 1
+
+# remove old generated phase_db files
+rm $file_path/*
+
+# run code
+python3 "$code_path"/detect_anomaly.py lasso ${calculate_error}
+
+# remove any generated files in current directory
+rm ./resource_agg
+rm ./threaddump_agg
