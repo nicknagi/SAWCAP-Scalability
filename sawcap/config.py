319a6b3daf67c1a5c954afe3c703ecf2da6541bc
runner <capstone>
2021-01-10 01:15:34 -0500

    emptied workers config

--- sawcap/config.py
+++ sawcap/config.py
@@ -1,6 +1,6 @@
 import logging
 
-WORKERS = ["172.31.15.58"] # List of ip addresses of the workers in the cluster 
+WORKERS = [""] # List of ip addresses of the workers in the cluster 
 INTERVAL = 2 # Time in seconds to wait to detect phase change
 LOG_LEVEL = logging.DEBUG # Log level for application
 STACK_SIM_THRESHOLD = 0.6 # Stacktrace similarity threshold to determine phase change
