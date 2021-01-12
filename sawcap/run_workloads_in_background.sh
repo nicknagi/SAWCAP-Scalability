8bfd0ff1410222615a53097331a15e4689ca9765
runner <capstone>
2021-01-12 18:20:43 -0500

    fix bug

--- sawcap/run_workloads_in_background.sh
+++ sawcap/run_workloads_in_background.sh
@@ -2,5 +2,6 @@
 
 # Redirects stderr and stdout to output.log
 mkdir -p ~/logs_data_collection && \
-    bash run_sawcap_on_workloads.sh &> /home/ubuntu/logs_data_collection/output.log & && \
-    echo "Started the workloads script in background check ~/logs_data_collection/output.log for logs"
+    bash run_sawcap_on_workloads.sh &> /home/ubuntu/logs_data_collection/output.log &
+
+echo "Started the workloads script in background check ~/logs_data_collection/output.log for logs"
