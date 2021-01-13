283b8e2f467731b3ab0edf22aafbe90a9cd3c664
Polina Govorkova <catchingthegist@gmail.com>
2021-01-13 02:32:58 -0500

    graphs from stats files. see command line parser for details.

---
+++ sawcap/graph_comparison.py
@@ -0,0 +1,98 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import pandasas pd
+
+def parse_stats_file(file, stat_names):
+  workload_dictionary = {}
+  current_workload_name = ""
+  current_workload_data = {}
+  for line in open(file):
+    if "sawcap problems" in line:
+      current_workload_data = {}
+    split = line.split()
+    if len(split)==1:
+      if current_workload_data != {}:
+        workload_dictionary[current_workload_name].append(current_workload_data)
+        current_workload_data = {}
+      current_workload_name = split[0]
+      if current_workload_name not in workload_dictionary.keys():
+        workload_dictionary[current_workload_name]=[]
+    else:
+      for stat_name in stat_names:
+        if stat_name in line:
+          current_workload_data[stat_name] = float(line.split(stat_name + " Prediction Accuracy: ")[1])
+          break
+  if current_workload_data != {}:
+    workload_dictionary[current_workload_name].append(current_workload_data)
+    current_workload_data = {}
+  lengths = [len(workload) for workload in workload_dictionary.values()]
+  number_of_workloads = lengths[0]
+  assert(len(set(lengths)) == 1)
+  return workload_dictionary, number_of_workloads
+
+def find_averages_per_workload_type(workload_dictionary, num_workloads, stat_names):
+  averages = {}
+  for stat_name in stat_names:
+    averages[stat_name] = {}
+  for worload_name, worload_stats in workload_dictionary.items():
+    for stat_name in stat_names:
+      averages[stat_name][worload_name] = 0
+    for stat_num, stat in enumerate(worload_stats):
+      for stat_name in stat_names:
+        averages[stat_name][worload_name] +=stat[stat_name]
+        if stat_num == (num_workloads-1):
+          averages[stat_name][worload_name] = averages[stat_name][worload_name]/num_workloads
+  return averages
+
+def plot_data(array_of_stats_for_algos, stat_names, algos, save_path):
+  merged_stats = {}
+  for stat_name in stat_names:
+    merged_stats[stat_name] = {}
+  for algo in array_of_stats_for_algos:
+    for stat_name in stat_names:
+      for worload_name, worload_stat in algo[stat_name].items():
+        if worload_name not in merged_stats[stat_name]:
+          merged_stats[stat_name][worload_name]=[]
+        merged_stats[stat_name][worload_name].append(worload_stat)
+  for stat_name in stat_names:
+    stat_frame = pd.DataFrame(merged_stats[stat_name]).T
+    stat_frame.columns = algos
+    stat_frame.plot(kind="bar")
+    # Add title and axis names
+    plt.title(stat_name)
+    plt.xlabel('Algoripthms')
+    plt.ylabel('Percentage Accuracy')
+    
+    # Limits for the Y axis
+    plt.ylim(0,100)
+
+    if save_path != None:
+      save_path = os.path.join(save_path, stat_name + '_'.join(algos) + '.png')
+      plt.savefig(save_path)
+      plt.close(fig)
+    else:
+      plt.show()
+
+plot_data([averages], stat_names, ["lasso"], None)
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Plot Stats')
+    parser.add_argument('--dir_name', required=True, help='Directory name with all files')
+    parser.add_argument('--stats', required=True, help='List of stats to compare, separated by a comma without space')
+    parser.add_argument('--stat_files', required=True, help='List of data files to compare, separated by a comma without space')
+    parser.add_argument('--algos', required=True, help='Algo names associated with stat files, , separated by a comma without space')
+    parser.add_argument('--save_path', help='Save path for graphs')
+    args = parser.parse_args()
+    
+    os.chdir(dir_name)
+    stat_names = args.stats.split(',')
+    stat_files = args.stat_files(',')
+    algos = args.algos(',')
+    array_of_stats_for_algos = []
+    for file in stat_files:
+	workload_dictionary, num_workloads = parse_stats_file(file, stat_names)
+	averages = find_averages_per_workload_type(workload_dictionary, num_workloads, stat_names)
+	array_of_stats_for_algos .append(averages)
+    plot_data(array_of_stats_for_algos, stat_names, algos, save_path)
