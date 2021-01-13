f1397d72893eac4b9d328713e3c4c84c722d55a2
Nekhil Nagia <nekhilnagia16@gmail.com>
2021-01-13 00:34:54 -0500

    add argument to script

--- sawcap/run_workloads_in_background.sh
+++ sawcap/run_workloads_in_background.sh
@@ -2,6 +2,6 @@
 
 # Redirects stderr and stdout to output.log
 mkdir -p ~/logs_data_collection && \
-    bash run_sawcap_on_workloads.sh &> /home/ubuntu/logs_data_collection/output.log &
+    bash run_sawcap_on_workloads.sh $1 &> /home/ubuntu/logs_data_collection/output.log &
 
 echo "Started the workloads script in background check ~/logs_data_collection/output.log for logs"
