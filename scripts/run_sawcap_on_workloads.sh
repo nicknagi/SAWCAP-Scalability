48fe08e6ef7eb72a2bf80621e8da851ff9046333
runner <capstone>
2021-01-26 19:29:00 -0500

    disabled most workloads for mock demo

--- scripts/run_sawcap_on_workloads.sh
+++ scripts/run_sawcap_on_workloads.sh
@@ -233,14 +233,14 @@ fi
 # run bayes
 # start_data_collection $bayes_name $bayes_prepare $bayes_run 
 
-# run nweight
-start_data_collection $nweight_name $nweight_prepare $nweight_run
+# # run nweight
+# start_data_collection $nweight_name $nweight_prepare $nweight_run
 
-# run kmeans
-start_data_collection $kmeans_name $kmeans_prepare $kmeans_run 
+# # run kmeans
+# start_data_collection $kmeans_name $kmeans_prepare $kmeans_run 
 
-# run pagerank
-start_data_collection $pagerank_name $pagerank_prepare $pagerank_run 
+# # run pagerank
+# start_data_collection $pagerank_name $pagerank_prepare $pagerank_run 
 
 # # run svm -- takes too long
 # start_data_collection $svm_name $svm_prepare $svm_run 
@@ -248,8 +248,8 @@ start_data_collection $pagerank_name $pagerank_prepare $pagerank_run
 # run wordcount
 start_data_collection $wordcount_name $wordcount_prepare $wordcount_run 
 
-# run rf
-start_data_collection $rf_name $rf_prepare $rf_run
+# # run rf
+# start_data_collection $rf_name $rf_prepare $rf_run
 
 original_code_stats_file=''
 if [ ! -z "${should_run_original_code}" ]
