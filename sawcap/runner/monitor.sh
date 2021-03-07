ccca7bce24ffda60a491ce4987e0eae4bbe502d6
runner <capstone>
2021-03-07 13:39:14 -0500

    use full path for gunicorn

--- sawcap/runner/monitor.sh
+++ sawcap/runner/monitor.sh
@@ -8,7 +8,7 @@ fi
 
 # Start API server to get worker data
 private_ip_address=$(ip addr show | grep -o "inet 192.[0-9]*\.[0-9]*\.[0-9]*" | grep -o "192.[0-9]*\.[0-9]*\.[0-9]*")
-gunicorn worker_data_api:app -b "$private_ip_address":8690 --reload --log-level DEBUG &> worker_data_api.log
+/home/ubuntu/.local/bin/gunicorn worker_data_api:app -b "$private_ip_address":8690 --reload --log-level DEBUG &> worker_data_api.log
 
 while true; do
 
