a0254d34fcc418267d5ddc254a0c838fd15ea7d4
Gerin Amalaraj <gerin.amalaraj6@gmail.com>
2021-03-11 23:36:53 -0500

    aggregate max, min, avg

---
+++ sawcap/metrics/metrics_query.py
@@ -0,0 +1,92 @@
+import logging
+import socket
+import argparse
+from influxdb import InfluxDBClient
+
+class MetricsQuery:
+    def __init__(self):
+        self.client = None
+        self.id_lookup = {
+            "baseline-test": ["2021-02-23T22:00:00Z", "2021-02-23T22:59:00Z"],
+            "ten-node-test": ["2021-02-23T22:15:00Z", "2021-02-23T22:53:00Z"],
+            "twenty-five-node-test": ["2021-03-04T19:39:00Z", "2021-03-04T20:19:00Z"],
+            "fifty-node-test": ["2021-03-04T21:23:00Z", "2021-03-04T21:55:00Z"],
+            "nn-test": ["2021-03-07T03:00:00Z", "2021-03-07T03:10:00Z"],
+        }
+        try:
+            self.client = InfluxDBClient( "192.168.0.3", 8086, database='metrics')
+            self.client.create_database('metrics')
+            self.hostname = str(socket.getfqdn())
+        except Exception as e:
+            logging.error(f"There was a problem connecting to InfluxDB at IP {ORCHESTRATOR_PRIVATE_IP} {e}")
+            logging.info("Will not publish statistics")
+
+    def get_max_val(self, id, metric):
+        """
+           id: uniqueId of experiment
+           metric: metric to look for (eg. 'cpu')
+        """
+        if id in self.id_lookup.keys():
+            experiment = self.id_lookup[id]
+        else:
+            logging.error(f"Unique id '{id}' not found")
+            return
+        t_start = experiment[0]
+        t_end = experiment[1]
+        print(f"start: {t_start}, end: {t_end}")
+        select_query = f'SELECT MAX("{metric}") from "sawcap_resource_consumption" WHERE time >= \'{t_start}\' AND time < \'{t_end}\';'
+        result = self.client.query(select_query)
+        data_points = list(result.get_points(measurement='sawcap_resource_consumption'))
+        print(f"Max {metric} for {id}: {data_points[0]['max']}")
+
+    def get_min_val(self, id, metric):
+        """
+           id: uniqueId of experiment
+           metric: metric to look for (eg. 'cpu')
+        """
+        if id in self.id_lookup.keys():
+            experiment = self.id_lookup[id]
+        else:
+            logging.error(f"Unique id '{id}' not found")
+            return
+        t_start = experiment[0]
+        t_end = experiment[1]
+        print(f"start: {t_start}, end: {t_end}")
+        select_query = f'SELECT MIN("{metric}") from "sawcap_resource_consumption" WHERE time >= \'{t_start}\' AND time < \'{t_end}\';'
+        result = self.client.query(select_query)
+        data_points = list(result.get_points(measurement='sawcap_resource_consumption'))
+        print(f"Min {metric} for {id}: {data_points[0]['min']}")
+
+
+    def get_avg_vals(self, id, metric):
+        if id in self.id_lookup.keys():
+            experiment = self.id_lookup[id]
+        else:
+            logging.error(f"Unique id '{id}' not found")
+            return
+        t_start = experiment[0]
+        t_end = experiment[1]
+        print(f"start: {t_start}, end: {t_end}")
+        select_query = f'SELECT AVG("{metric}") from "sawcap_resource_consumption" WHERE (time >= \'{t_start}\' AND time < \'{t_end}\') GROUP BY time(5m);'        
+        result = self.client.query(select_query)
+        data_points = list(result.get_points(measurement='sawcap_resource_consumption'))
+        for entry in data_points:
+            print(f"{entry['time']}: {entry['mean']:.5f}")
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-uid", "--uniqueid", help="Unique Id of experiment")
+parser.add_argument("-m", "--metric", help="Metric to look for cpu, mem, download, upload etc.")
+parser.add_argument("-op", "--operation", help="Operation to perform max, min, avg etc.")
+args = parser.parse_args()
+
+metrics_query = MetricsQuery()
+
+if args.operation == 'max':
+    metrics_query.get_max_val(args.uniqueid, args.metric)
+elif args.operation == 'min':
+    metrics_query.get_min_val(args.uniqueid, args.metric)
+elif args.operation == 'avg':
+    metrics_query.get_avg_vals(args.uniqueid, args.metric)
+else:
+    logging.error(f"Invalid input")
+
